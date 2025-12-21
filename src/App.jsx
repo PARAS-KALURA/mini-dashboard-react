9bb1e5cc2140eea4cac6d1c75e767259f1462888
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-21 10:46:17 +0530

    UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <aside
+    className='min-h-screen w-60 bg-blue-600'
+    ></aside>
+    </>
+  )
+}
+
+export default App
