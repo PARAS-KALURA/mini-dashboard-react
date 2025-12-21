6448d975727435e9fc6c7372706586a5a3e13932
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-21 10:58:11 +0530

    aside done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ const App = () => {
   return (
     <>
     <aside
-    className='min-h-screen w-60 bg-blue-600'
-    ></aside>
+    className='min-h-screen w-60 bg-blue-600 text-white'
+    >
+      <h2 className='text-center pt-6  pb-8 text-2xl font-bold ' >My App</h2>
+      <ul className='text-center p-4 flex gap-4 flex-col ' >
+
+        <li className='border border-blue-900 rounded-sm px-2 py-1 cursor-pointer hover:scale-110 transition-transform' >Home</li>
+
+        <li className='border border-blue-900 rounded-sm px-2 py-1 cursor-pointer hover:scale-110 transition-transform' >Dashboard</li>
+        
+        <li className='border border-blue-900 rounded-sm px-2 py-1 cursor-pointer hover:scale-110 transition-transform' >Task</li>
+
+        <li className='border border-blue-900 rounded-sm px-2 py-1 cursor-pointer hover:scale-110 transition-transform' >Setting</li>
+
+      </ul>
+    </aside>
     </>
   )
 }
