9689e605154fd4a9b52e3817b892bf4da4d1053e
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-23 03:53:53 +0530

    OnGoing

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Task = () => {
+  
+  const [task, setTask] = useState([]);
+
+  const addTask = () => {
+    console.log("Added");
+    
+  }
+
   return (
     <div className=' px-6 pt-10 mb-10 max-w-4xl' >
     {/* Header */}
@@ -8,20 +16,29 @@ const Task = () => {
       <div className='space-y-1' >
       <h3 className='text-2xl font-semibold' >Task</h3>
       <p className='text-sm text-gray-500'>Manage and track your tasks</p>
+
       </div>
       
       <div>
-        <button className='bg-blue-500 px-4 py-2 text-white rounded cursor-pointer hover:bg-blue-600' >+New Task</button>
+        <button  onClick={addTask} className='bg-blue-500 px-4 py-2 text-white rounded cursor-pointer hover:bg-blue-600' >+Add Task</button>
       </div>
     </header>
 
-    {/* Tasks */}
 
-    <div className='divide-y border border-gray-300 mt-10' >
-      <p className='' >1</p>
-      <p>2</p>
-      <p>3</p>
+    {/* Input */}
+
+    <div>
+      <input className='w-full px-4 py-2 mt-4 '  type="text" placeholder='Add Task' value={task} onChange={(e) => setTask(e.target.value)} />
     </div>
+
+    {/* Tasks */}
+ 
+    <div className="divide-y border border-gray-300 rounded-md mt-2">
+  <p className="p-3">1</p>
+  <p className="p-3">2</p>
+  <p className="p-3">3</p>
+</div>
+
 
     </div>
   )
